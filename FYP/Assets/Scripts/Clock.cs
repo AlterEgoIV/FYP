b84ae5571e95c0a6da010779a53807b033675c5d
Carl Kavanagh <alterego01010@gmail.com>
2018-11-13 20:17:22 +0000

    Added EnemyAI and Clock scripts

---
+++ FYP/Assets/Scripts/Clock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public float elapsedTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        elapsedTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ++elapsedTime;
+    }
+
+    public void ResetTime()
+    {
+        elapsedTime = 0f;
+    }
+
+    public float GetElapsedTime()
+    {
+        return elapsedTime;
+    }
+}
