28c174b6b34c57fe6e18d4b988b1660055b30569
Carl Kavanagh <alterego01010@gmail.com>
2019-04-08 19:40:32 +0100

    Fired bullets are removed after a period of time

--- FYP/Assets/Scripts/Travel.cs
+++ FYP/Assets/Scripts/Travel.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Travel : MonoBehaviour {
 
     public float speed = 20f;
+    int elapsedTime;
 
 	// Use this for initialization
 	void Start () {
-		
+        elapsedTime = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if(elapsedTime >= 180)
+        {
+            Destroy(gameObject);
+        }
+
+        ++elapsedTime;
 	}
 
     void OnCollisionEnter(Collision collision)
